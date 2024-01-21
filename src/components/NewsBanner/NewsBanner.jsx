0147943bcd7e8d3e3c1f112d949643e77edb6814
nata-naumova <naumonata@yandex.ru>
2024-01-21 16:28:57 +0300

    git init

---
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "../Image/Image";
+
+import { formatTimeAgo } from "../../helpers/formatTimeAgo";
+
+import styles from "./styles.module.css";
+
+const NewsBanner = ({ item }) => {
+  return (
+    <div className={styles.banner}>
+      <Image image={item?.image} />
+      <h3 className={styles.title}>{item.title}</h3>
+      <p className={styles.extra}>
+        {formatTimeAgo(item.published)} by {item.auhtor}
+      </p>
+    </div>
+  );
+};
+
+export default NewsBanner;
